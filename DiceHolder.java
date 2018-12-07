e376d68f5a7157f31b7c8a348a3a7cd88dc6d582
RohanC <Rohan.chappidi@gmail.com>
2018-12-07 09:59:33 -0500

    Update DiceHolder.java

--- DiceHolder.java
+++ DiceHolder.java
@@ -3,12 +3,14 @@ public class DiceHolder{
 
 	private ArrayList <Die> holder;
 
+	//Instance variable
 	Die die = new Die();
 
+	//default constructor
 	public DiceHolder(){
 		holder = new ArrayList <Die>();
 	}
-
+	//Adding the dice method
 	public int addDie(Die die){
 		if(holder.size()!=6){
 			holder.add(die);
@@ -16,7 +18,7 @@ public class DiceHolder{
 	}
 		return -1;
 	}
-
+	//Shaking all the dice method
 	public void shake(){
 		for(int i = 0; i<holder.size(); i++){
 			holder.get(i).roll();
@@ -24,7 +26,7 @@ public class DiceHolder{
 		}
 
 	}
-
+	//output
 	public String toString(){
 		for(int i = 0; i<holder.size(); i++)
 			System.out.println(holder.get(i)+"\n");
